aa6b97d2fc9851c88b526562be9a536d4b6de9a2
Russell Ryan <rryan@duckdunn.local>
2019-07-17 15:27:38 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+import pylinear.__init__ as info
+
+setup(name='pylinear',\
+      version=info.__version__,\
+      author=info.__author__,\
+      author_email=info.__email__,\
+      keywords='grism linear python',\
+      description=info.__description__,\
+      license=info.__license__,\
+      entry_points={'console_scripts':['pylinear=pylinear.linear:main']},
+      install_requires=['numpy','scipy','ruamel.yaml','emcee',\
+                        'polyclip','h5axeconfig','argparse','astropy'],\
+      classifiers=['Development Status :: 1 Planning',\
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Astronomy',],\
+      packages=find_packages(),\
+      package_data={'pylinear':['config/defaults.yml','synthphot/filters/*filt']})
