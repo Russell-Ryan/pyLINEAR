ee4341aaafd0afb0c67093c14683aa288918eeeb
Russell Ryan <rryan@duckdunn.local>
2019-08-12 12:09:57 -0400

    added file convexhull.py

---
+++ pylinear/utilities/convexhull.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+from scipy.spatial import ConvexHull
+
+
+def vertices(x,y):
+    dx=[0,0,1,1]
+    dy=[0,1,1,0]
+    xy=[]
+    for ddx,ddy in zip(dx,dy):
+        xx=x+ddx
+        yy=y+ddy
+        xy.extend(list(zip(xx,yy)))
+    xy=np.array(list(set(xy)))
+
+    hull=ConvexHull(xy)
+    
+    xh,yh=[],[]
+    for vertex in hull.vertices:
+        xh.append(xy[vertex,0])
+        yh.append(xy[vertex,1])
+    xh,yh=np.array(xh),np.array(yh)
+
+    return xh,yh
+    
+
+    
+    
