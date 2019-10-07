bffb4aa1ac52eb91077009f608ead140c5d71c9a
Russell Ryan <rryan@duckdunn.stsci.edu>
2019-10-07 11:55:38 -0400

    fixed entry points

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup,find_packages
+tfrom setuptools import setup,find_packages
 import os
 
 
@@ -8,28 +8,35 @@ info = {
     '__author__': AUTHOR,
     '__version__': '1.0',
     '__maintainer__': AUTHOR,
-    '__email__': 'rryan@stsci.edu',
+    '__author_email__': 'rryan@stsci.edu',
     '__license__': 'MIT',
     '__cite__': 'https://ui.adsabs.harvard.edu/abs/2018PASP..130c4501R/abstract',
+    '__keywords__':'grism linear WFC3 HST',
     '__description__':'Linear Extraction and Simulation of Slitless Spectroscopy',
     '__credits__': AUTHOR
 }
 
 # Generate package metadata
-with open(os.path.join(PKG, 'info.py'), 'w+') as fp:
-    for k, v in info.items():
-        fp.write('{} = "{}"{}'.format(k, v, os.linesep))
+#with open(os.path.join(PKG, 'info.py'), 'w+') as fp:
+#    for k, v in info.items():
+#        fp.write('{} = "{}"{}'.format(k, v, os.linesep))
 
+
+
+
+#https://docs.python.org/2/distutils/setupscript.html
 # call SETUP
 setup(name=PKG,
       version=info['__version__'],
       author=info['__author__'],
-      author_email=info['__email__'],
-      keywords='grism linear',
-      description=info['__description__'],
+      author_email=info['__author_email__'],
+      keywords=info['__keywords__'],
+      long_description=info['__description__'],
+      maintainer=info['__maintainer__'],
       license=info['__license__'],
-      setup_requires=['wget'],
+      #setup_requires=['wget'],
       install_requires=['numpy','astropy','numpy','h5axeconfig @ git+https://github.com/russell-ryan/h5axeconfig#egg=h5axeconfig','emcee','ruamel.yaml','pkginfo','shapely'],
+      entry_points={'console_scripts':['pylinear=pylinear.linear:main']},
       classifiers=['Development Status :: 5 Production/Stable',
                    'Intended Audience :: Science/Research',
                    'Topic :: Scientific/Engineering :: Astronomy',],
