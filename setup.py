d7c3f1e943ce81dd40c9f00430e09335a40897cd
Russell Ryan <rryan@duckdunn.stsci.edu>
2019-10-07 16:03:53 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,40 +1,22 @@
 from setuptools import setup,find_packages
 import os
 
-
-PKG='pylinear'
-AUTHOR='Russell Ryan'
-info = {
-    '__author__': AUTHOR,
-    '__version__': '1.0',
-    '__maintainer__': AUTHOR,
-    '__author_email__': 'rryan@stsci.edu',
-    '__license__': 'MIT',
-    '__cite__': 'https://ui.adsabs.harvard.edu/abs/2018PASP..130c4501R/abstract',
-    '__keywords__':'grism linear WFC3 HST',
-    '__description__':'Linear Extraction and Simulation of Slitless Spectroscopy',
-    '__credits__': AUTHOR
-}
-
-# Generate package metadata
-#with open(os.path.join(PKG, 'info.py'), 'w+') as fp:
-#    for k, v in info.items():
-#        fp.write('{} = "{}"{}'.format(k, v, os.linesep))
-
+ld='The python implementation of the LINEAR algorithm as developed Ryan, Casertano, & Pirzkal 2018: https://ui.adsabs.harvard.edu/abs/2018PASP..130c4501R/abstract'
 
 
 
 #https://docs.python.org/2/distutils/setupscript.html
-# call SETUP
-setup(name=PKG,
-      version=info['__version__'],
-      author=info['__author__'],
-      author_email=info['__author_email__'],
-      keywords=info['__keywords__'],
-      long_description=info['__description__'],
-      maintainer=info['__maintainer__'],
-      license=info['__license__'],
-      #setup_requires=['wget'],
+setup(name='pylinear',
+      version='1.0',
+      author='Russell Ryan',
+      author_email='rryan@stsci.edu',
+      keywords='grism linear WFC3 HST',
+      description='Linear Reconstruction of Slitless Spectroscopy',
+      long_description=ld,
+      maintainer='Russell Ryan',
+      license='MIT',
+      url='https://github.com/Russell-Ryan/pyLINEAR',
+      platforms='posix',
       install_requires=['numpy','astropy','numpy','h5axeconfig @ git+https://github.com/russell-ryan/h5axeconfig#egg=h5axeconfig','emcee','ruamel.yaml','pkginfo','shapely'],
       entry_points={'console_scripts':['pylinear=pylinear.linear:main']},
       classifiers=['Development Status :: 5 Production/Stable',
