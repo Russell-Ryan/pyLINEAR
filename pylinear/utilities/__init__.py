781497b82e52385e5ed81def3981ebcff6e4d70f
Russell Ryan <rryan@duckdunn.local>
2019-10-06 20:04:18 -0400

    exposed pool class

--- pylinear/utilities/__init__.py
+++ pylinear/utilities/__init__.py
@@ -4,5 +4,6 @@ from .pkginfo import pkginfo
 from .progressbar import ProgressBar
 from .asciitable import AsciiTable
 from .indexeddict import IndexedDict
+from .pool import Pool
 #from .ncpus import nCPUs
 from . import gzip
