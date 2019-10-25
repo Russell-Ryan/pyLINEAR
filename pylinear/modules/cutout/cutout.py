c9ad025f99236ab4155781999673965f0df10bf7
Russell Ryan <rryan@duckdunn.local>
2019-10-24 23:53:49 -0400

    used FITSImage to make the cutouts

--- pylinear/modules/cutout/cutout.py
+++ pylinear/modules/cutout/cutout.py
@@ -37,31 +37,33 @@ def cutoutWorker(flt,conf,grismconf,sources):
                     srcname=str(segid)
                     ddt=h5table.DDT(src.segid)
                     ddt.readH5(h5beamIn)
-                    
-                    xg,yg=indices.one2two(ddt.xyg,detimg.shape)
+                                        
                     if len(ddt)>0:
+                        xg,yg=indices.one2two(ddt.xyg,detimg.shape)
+                        x0,x1=np.amin(xg)-xpad,np.amax(xg)+xpad
+                        y0,y1=np.amin(yg)-ypad,np.amax(yg)+ypad
 
-
-                        print("USE FITSIMAGE TO CUT STAMP")
+                        # cut the stamp
+                        sub=sci.extract(x0,x1,y0,y1)
                         
-                        x0=np.clip(np.amin(xg)-xpad-1,0,detimg.shape[1])
-                        x1=np.clip(np.amax(xg)+xpad,0,detimg.shape[1])
-                        y0=np.clip(np.amin(yg)-ypad-1,0,detimg.shape[0])
-                        y1=np.clip(np.amax(yg)+ypad,0,detimg.shape[0])
-
-                        sub=sci[y0:y1,x0:x1]
-                        #hdr=scihdr.copy()
-                        hdr=sci.header.copy()
-                        hdr['NAXIS1']=x1-x0
-                        hdr['NAXIS2']=y1-y0
-                        hdr['CRPIX1']-=x0
-                        hdr['CRPIX2']-=y0
-                        hdr['LTV1']=-x0
-                        hdr['LTV2']=-y0
-
+                        #x0=np.clip(np.amin(xg)-xpad-1,0,detimg.shape[1])
+                        #x1=np.clip(np.amax(xg)+xpad,0,detimg.shape[1])
+                        #y0=np.clip(np.amin(yg)-ypad-1,0,detimg.shape[0])
+                        #y1=np.clip(np.amax(yg)+ypad,0,detimg.shape[0])
+                        #
+                        #sub=sci[y0:y1,x0:x1]
+                        ##hdr=scihdr.copy()
+                        #hdr=sci.header.copy()
+                        #hdr['NAXIS1']=x1-x0
+                        #hdr['NAXIS2']=y1-y0
+                        #hdr['CRPIX1']-=x0
+                        #hdr['CRPIX2']-=y0
+                        #hdr['LTV1']=-x0
+                        #hdr['LTV2']=-y0
 
                         
-                        d=h5beamOut.create_dataset(srcname,data=sub,\
+                        # write the file to disk
+                        d=h5beamOut.create_dataset(srcname,data=sub.image,\
                                                    compression='gzip')
                         d.attrs['x0']=np.uint16(x0)
                         d.attrs['x1']=np.uint16(x1)
