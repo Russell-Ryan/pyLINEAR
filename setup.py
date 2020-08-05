0d94f534bb87793cd0ed69e1ee9cfc6bca9e0e8b
Russell Ryan <rryan@jamerson.local>
2020-08-04 21:23:06 -0400

    removed excess in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup,find_packages
-#import os
 
 
 
@@ -15,8 +14,6 @@ reqs=['numpy>1.16','scipy','astropy','emcee','h5py',
       'colorlog @ {}colorlog'.format(rryan),
       'polyclip @ {}polyclip'.format(rryan)]
       
-      
-#'h5axeconfig @ git+https://github.com/russell-ryan/h5axeconfig'.format(rryan),
 
 
 
@@ -41,25 +38,4 @@ setup(name='pylinear',
       package_data={'pylinear':['sedphot/filters/*filt',
                                 'grism/instruments/instruments.xml']})
 
-                                 
-#'grism/siaf/siaf.h5']})
-                        
-
-#from setuptools import setup,find_packages
-#import pylinear.__init__ as info
-#
-#setup(name='pylinear',\
-#      version=info.__version__,\
-#      author=info.__author__,\
-#      author_email=info.__email__,\
-#      keywords='grism linear python',\
-#      description=info.__description__,\
-#      license=info.__license__,\
-#      entry_points={'console_scripts':['pylinear=pylinear.linear:main']},
-#      install_requires=['numpy','scipy','ruamel.yaml','emcee','shapely',\
-#                        'polyclip','h5axeconfig','argparse','astropy'],\
-#      classifiers=['Development Status :: 4 Beta',\
-#                   'Intended Audience :: Science/Research',
-#                   'Topic :: Scientific/Engineering :: Astronomy',],\
-#      packages=find_packages(),\
-#      package_data={'pylinear':['config/defaults.yml','synthphot/filters/*filt']})
+              
